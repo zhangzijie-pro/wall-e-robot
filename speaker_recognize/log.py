10dd739f1f28c5554246784afaaf08816f520cb6
zhangzijie <zzj01262022@163.com>
2025-05-06 20:22:11 +0800

    _log

--- speaker_recognize/log.py
+++ speaker_recognize/log.py
@@ -1,9 +1,41 @@
 import logging
-logger = logging.getLogger(__name__)
-# logger.setLevel(level=logging.INFO)
-handler = logging.FileHandler("speaker_embedding.log")
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s -%(message)s')
-handler.setFormatter(formatter)
+import functools
+import os
 
-logger.addHandler(handler)
+class CustomLogger(logging.Logger):
+    def findCaller(self, stack_info=False, stacklevel=1):
+        # 向上查找真实调用 logger.info 的函数
+        f = logging.currentframe()
+        for _ in range(stacklevel):
+            if f is not None:
+                f = f.f_back
+        while f:
+            co = f.f_code
+            filename = os.path.normcase(co.co_filename)
+            if "logging" not in filename:
+                return (filename, f.f_lineno, co.co_name, None)
+            f = f.f_back
+        return ("<unknown>", 0, "<unknown>", None)
+
+def logger_config(name="custom_logger", log_file="speaker_embedding.log"):
+    logging.setLoggerClass(CustomLogger)
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        handler = logging.FileHandler(log_file)
+        formatter = logging.Formatter('%(asctime)s - %(filename)s - %(funcName)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
+
+logger = logger_config()
+
+def log():
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
